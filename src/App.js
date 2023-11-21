a78be509ada601fe90535fc41bb1a232510c4bc5
Adarsh <adarshnallamothu3@gmail.com>
2023-11-21 19:44:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { TabMenu } from 'primereact/tabmenu';
 
-function App() {
+import About from './About';
+import Homes from './Homes';
+import Service from './Service';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+
+const App = () => {
+  const [activeIndex, setActiveIndex] = useState(3);
+  console.log(activeIndex)
+  switch (activeIndex) {
+    case 0:
+      return <Homes/>
+    case 1:
+      return <About/>
+    case 2:
+      return <Service/>
+    case 3 :
+      return <Portfolio/>
+    case 4:
+      return <Contact/>
+    default:
+      break;
+  }
+  const items = [
+    {label: 'Home'},
+    {label: 'About'},
+    {label: 'Service'},
+    {label: 'Portfolio'},
+    {label: 'Contact'}
+];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactjs
-        </a>
-      </header>
-    </div>
-  );
+    <div className="card">
+    <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}/>
+</div>
+  )
 }
 
-export default App;
+export default App
